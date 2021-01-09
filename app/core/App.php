e047e62a045767490f35a1708c1d11e16bffca59
attaqwadepok <77178155+attaqwadepok@users.noreply.github.com>
2021-01-09 17:27:59 +0700

    Upload MVC

---
+++ app/core/App.php
@@ -0,0 +1,23 @@
+<?php
+
+class App
+{
+
+  protected $controller;
+
+  public function __construct()
+  {
+    $url = $this->parseURL();
+    var_dump($url);
+  }
+
+  public function parseURL()
+  {
+    if (isset($_GET['url'])) {
+      $url = rtrim($_GET['url'], '/');
+      $url = filter_var($url, FILTER_SANITIZE_URL);
+      $url = explode('/', $url);
+      return $url;
+    }
+  }
+}
